fd77c133eb6fe75bf03210efa3f795093c2091c7
PAK <51408093+teltors@users.noreply.github.com>
2019-11-28 14:05:53 +0900

    Add files via upload

---
+++ board/board.sql
@@ -0,0 +1,11 @@
+create table board (
+	num int not null auto_increment,
+	id varchar(10) not null,
+	name varchar(10) not null,
+	subject varchar(100) not null,
+	content text not null,
+	regist_day varchar(30),
+	hit int,
+	ip varchar(20),
+	primary key(num)
+)default charset=utf8;
